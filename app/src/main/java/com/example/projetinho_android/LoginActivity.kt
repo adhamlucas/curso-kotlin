38f2d60d3e3541df2d8510886ad54b4918cb6edc
Adham Lucas <adhamlucas20@gmail.com>
2019-04-08 17:17:59 -0400

    base criada

--- app/src/main/java/com/example/projetinho_android/LoginActivity.kt
+++ app/src/main/java/com/example/projetinho_android/LoginActivity.kt
@@ -1,7 +1,14 @@
 package com.example.projetinho_android
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
 }
