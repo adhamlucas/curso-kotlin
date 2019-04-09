683de1b19514bdc43c56cbfb25ca17eedfc10b77
Adham Lucas <adhamlucas20@gmail.com>
2019-04-09 18:06:50 -0400

    Upgrade register Activity

--- app/src/main/java/com/example/projetinho_android/MainActivity.kt
+++ app/src/main/java/com/example/projetinho_android/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,23 +19,30 @@ class MainActivity : AppCompatActivity() {
             val email = email_edittext_register.text.toString()
             val password = password_edittext_register.text.toString()
 
+            if(email.isEmpty()|| password.isEmpty()){
+                Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             Log.d("MainActivity", "Emails is: " + email)
             Log.d("MainActivity", "Password is:  $password")
 
-            //Firebase Authenticate to Create a user with email and password
 
+            //Firebase Authenticate to Create a user with email and password
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener {
-                    if(!it.isSuccessful) return@addOnCompleteListener
-
-                    Log.d("Main", "Sucessufly create user with uid: ${it.result.user.uid}")
+                    if(it.isSuccessful){
+                        Log.d("MainActivity", "Sucessufly create user with uid: ${it.result?.user?.uid}")
+                    } //
+                    else{
+                        return@addOnCompleteListener
+                    }
                 }
-//            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
-//                .addOnCompleteListener {
-//                    if(!it.isSuccessful) return@addOnCompleteListener
+                .addOnFailureListener {
+                    Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+//                    Log.d("MainActivity", "Failed to create user: ${it.message}")
+                }
 //
-//                    Log.d("Main", "Sucessfully created user with uid: ${it.result.user.uid}")
-//                }
         }
 
         already_have_account_text_view.setOnClickListener{
