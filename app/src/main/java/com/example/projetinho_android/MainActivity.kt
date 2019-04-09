a7fb876953268d8932299351e0ae315f81e4d6dd
Adham Lucas <adhamlucas20@gmail.com>
2019-04-09 09:08:06 -0400

    Adicionando Firebase ao projeto e MainAcitivity (registro)

--- app/src/main/java/com/example/projetinho_android/MainActivity.kt
+++ app/src/main/java/com/example/projetinho_android/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,21 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("MainActivity", "Emails is: " + email)
             Log.d("MainActivity", "Password is:  $password")
+
+            //Firebase Authenticate to Create a user with email and password
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main", "Sucessufly create user with uid: ${it.result.user.uid}")
+                }
+//            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener {
+//                    if(!it.isSuccessful) return@addOnCompleteListener
+//
+//                    Log.d("Main", "Sucessfully created user with uid: ${it.result.user.uid}")
+//                }
         }
 
         already_have_account_text_view.setOnClickListener{
